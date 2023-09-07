4f2c18468f294c62737b08b4f5610da93dca6b71
Emir Marques <emirdeliz@gmail.com>
2023-09-07 13:04:28 -0300

    feat: Fixed the day button size

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -3,6 +3,10 @@ import { ButtonProps } from './Button';
 
 export const Button = styled.TouchableOpacity<ButtonProps>`
   opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Label = styled.Text<{ fontWeight?: 'bold' | 'normal' }>`
